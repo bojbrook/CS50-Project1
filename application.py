a6fb2373c144ef1a082a62a1bac825ceba109d11
Bowen Brooks <bowenjbrooks@gmail.com>
2018-09-24 08:42:08 -0700

    added individual book page

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template ,session
+from flask import Flask, render_template ,session, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,20 +23,38 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    books = db.execute("select * from books")
-    return render_template("index.html", books=books)
-
-@app.route("/books")
-def books():
     sql_command = "SELECT title, name FROM books INNER JOIN authors on books.author_id = authors.id"
     books = db.execute(sql_command)
     return render_template("index.html", books=books)
 
-@app.route("/search/<string:item>")
-def search(item):
-    sql_command = f"SELECT title, name FROM books INNER JOIN authors on books.author_id = authors.id AND books.isbn='{item}'"
-    if db.execute(sql_command).rowcount == 0:
-        return render_template("error.html", message="No book found")
+@app.route("/books/<string:title>")
+def book_info(title):
+    return render_template("book.html", title=title)
+
+# Search for individual book
+@app.route("/search", methods=["GET"])
+def search():
+    if request.method == 'GET':
+        item =  request.args.get("input-book-search")
+        #
+        print (item)
+        #
+        # search a book
+        sql_command_isbn = f"SELECT title, name FROM books INNER JOIN authors on books.author_id = authors.id AND books.isbn = '{item}'"
+        sql_command_title = f"SELECT title, name FROM books INNER JOIN authors on books.author_id = authors.id AND books.title LIKE '%{item}%'"
+        sql_command_author = f"Select title, name from books INNER JOIN authors on books.author_id = authors.id AND authors.name LIKE '%{item}%'"
+
+        if db.execute(sql_command_title).rowcount == 0 and db.execute(sql_command_isbn).rowcount == 0 and db.execute(sql_command_author) == 0:
+            return render_template("error.html", message="No book found")
+        else:
+            books = []
+            if(db.execute(sql_command_title).rowcount > 0):
+                books = books + list(db.execute(sql_command_title))
+            if(db.execute(sql_command_isbn).rowcount > 0):
+                books = books + list(db.execute(sql_command_isbn))
+            if(db.execute(sql_command_author).rowcount > 0):
+                books = books + list(db.execute(sql_command_author))
+            print(books)
+            return render_template("index.html", books=books)
     else:
-        books = db.execute(sql_command)
-        return render_template("index.html", books=books)
+        return render_template("error.html", message="Not correct message")
