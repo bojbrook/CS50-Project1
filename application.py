6decfde4b1fa382ecbc8b7528eea62a47e0fc010
Bowen Brooks <bowenjbrooks@gmail.com>
2018-09-28 09:31:20 -0700

    made sign up page add user to database

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, render_template ,session, request, redirect
+from flask import Flask, render_template ,session, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
@@ -35,6 +37,25 @@ def sign_up():
     return render_template("sign_up.html")
 
 
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    name = request.form.get("inputName")
+    userName = request.form.get("inputUserName")
+    password = sha256_crypt.encrypt(request.form.get("inputPassword"))
+
+    print(f"Name: {name} Username: {userName} Hash: {password}")
+    sql_check_user_command = f"SELECT * FROM users WHERE username='{userName}'"
+    print (password)
+    
+    if(db.execute(sql_check_user_command).rowcount > 0):
+        return "Username is in use"
+
+    insert_user_sql_command = "INSERT INTO users (name, username, password) VALUES (:name,:userName,:password)"
+    db.execute(insert_user_sql_command,{"name": name, "userName": userName, "password": password})
+    db.commit()
+
+    return redirect(url_for('sign_up'))
+    # return redirect(url_for('index'))
 @app.route("/login", methods=["POST"])
 def login():
     if request.form['InputPassword'] == 'password' and request.form['InputUsername'] == 'bojbrook':
